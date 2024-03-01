f510b72b04b2b73f09c45079735455b399ccced7
Ines-05 <inestchekpo5@gmail.com>
2024-03-01 20:29:36 +0100

    delete function

--- class.py
+++ class.py
@@ -15,29 +15,17 @@ def remplir_liste():
         if name in list_class:
             print("Name already in the list")
         else:
-            matricule=input("Veuiller entre le matricule de l'etudiant : ")
+            matricule=int(input("Veuiller entre le matricule de l'etudiant : "))
             if matricule in matricules:
                 print("Ce matricule appartient deja à un autre etudiant.")
+                matricule=int(input("Veuillez choisir un autre matricule : "))
             else:
                 matricules.append(matricule)
                 list_class.append(name)  
             
     list_sorted=sorted(list_class)
-    return list_sorted
+    return list_sorted,matricules
 
-def ajouter_etu():
-    name=input("Entrer le nom et le prenom du nouvel étudiant : ")
-    matricule=input("Veuillez précisez le matricule de l'etudiant : ")
-    if matricule not in matricules:
-        matricules.append(matricule)
-    else:
-        print("Matricule already taken")
-    if name not in list_class:
-        list_class.append(name)
-        list_sorted=sorted(list_class)
-        return list_sorted
-    else:
-        return False
     
 def saisir_note():
     notes_etu=[]
@@ -58,91 +46,41 @@ def saisir_note():
 def calcul_mat(notes_etu):#calculer moyenne matiere
     matiere=[]
     for note in notes_etu:
-        mat=[]
         for i in range(0,len(note),2):
+            som=0
             som=note[i]+note[i+1]
             moy_mat=som/2
-            mat.append(moy_mat)
-        matiere.append(mat)
+        matiere.append(moy_mat)
     return matiere
 
 def calculer_moyG(matiere):#calculer moyenne géneral
-    moyenne_G=[]
+    s=0
+    nbr_note=0
     for n in matiere:
-        # print(n)
-        s=sum(n)
-        moy_G=s/len(n)
-        moyenne_G.append(moy_G)
-    return moyenne_G
+        s+=n
+        nbr_note+=1
+    moy_G=s/nbr_note
+    return moy_G
 
-def sauvegarder_fichier_csv(noms, notes_etudiants):
-    with open("notes_etudiants.csv",mode="a+",newline='') as file :
-        writer = csv.writer(file)
-
-        header=["Nom et prenom"]
-
-        for i in range(len(notes_etudiants[0])//2):
-            header.append(f"Matiere {i+1} _Note 1 ")
-            header.append(f"Matiere {i+1} _Note 2 ")
-        header.append("Moyenne")
-
-        writer.writerow(header)
-
-        for i in range(len(noms)):
-            ligne=[noms[i]]
-            for j in range(0,len(notes_etudiants[i]),2):
-                note1=notes_etudiants[i][j]
-                note2=notes_etudiants[i][j+1]
-                ligne.extend([note1,note2])
-
-            moyenne=calculer_moyG([notes_etudiants[i]])[0]
-            ligne.append(moyenne)
-            writer.writerow(ligne)
-
-
-
-# def rank(list_etudiants,moyenne):
-#     etudiant=list(zip(list_etudiants,moyenne))
-
-#     etudiant_tries= sorted(etudiant, key=lambda x:x[1] , reverse=True)
-#     rang={}
-#     for i ,(nom,moyenne) in enumerate(etudiant_tries,start=1):
-#         rang[nom]=i
-#     return rang
 
 
 def mention(moy_g):
     if moy_g > 0 and moy_g < 10:
-        print("Mention : Mediocre")
+        return "Mention : Mediocre"
     
     elif (moy_g >=10 and moy_g <12):
-        print("Mention : Insuffisant")
+        return "Mention : Insuffisant"
 
     elif (moy_g >=12 and moy_g <14):
-        print("Mention : Assez-bien")
+        return "Mention : Assez-bien"
     
     elif (moy_g >=14 and moy_g <16):
-        print("Mention : Bien")
+            return "Mention : Bien"
 
     elif (moy_g >=16 and moy_g <18):
-        print("Mention : Très-bien")  
+        return "Mention : Très-bien"
     else :
-        print("Mention : Excellente")
-
-
-def remplir_information():
-    student_dictionnary={}
-    for i in list_class[:nbr_remplir]:
-        note_list=saisir_note()
-    #creer un dictionnaire qui prend comme valeur le nom et le prenom de l'etudiant et comme valeur une liste de ces notes par matiere
-    #student_dictionnary=dict(zip(list_class,note_list))
-        student_dictionnary[i]=note_list
-
-
-    for x,y in student_dictionnary.items():
-        print(x,y)
-
-    return student_dictionnary
+        return "Mention : Excellente"
 
 
 def moyenne_classe_minmax(effectif,moyenne):
@@ -153,38 +91,51 @@ def moyenne_classe_minmax(effectif,moyenne):
         M=somme/effectif
     return M,Moyenne_min ,Moyenne_max
 
-
-# list_class=remplir_liste()
-# # student=remplir_information()
-
-# # print(list_class)
-
-# #saisir les notes des étudiants
-# notes_etu=saisir_note()
-
-# #calcul des moyennes par matiere
-# matieres=calcul_mat(notes_etu)
-
-# #calcul des moyennes génerales 
-# moyennes=calculer_moyG(matieres)
-
-# # sauvegarder_fichier_csv(list_class,notes_etu)
-
-# # Collecte des informations des étudiants
-# etudiants = remplir_information()
-
-# # Récupération des noms des étudiants et de leurs notes
-# noms_etudiants = list(etudiants.keys())
-# notes_etudiants = list(etudiants.values())
-
-# # Appel de la fonction pour sauvegarder les données dans un fichier CSV
-# sauvegarder_fichier_csv(noms_etudiants, notes_etudiants)
+import pandas as pd 
+def remplir_information(list_class):
+    student_dictionnary=[]
+    for etudiant, matricule in zip(list_class[:nbr_remplir],matricules[:nbr_remplir]):
+        note_list=saisir_note()
+        moy_mat=calcul_mat(note_list)
+        print(moy_mat)
+        moy_G=calculer_moyG(moy_mat)
+        print(moy_G)
+        mentions=mention(moy_G)
+    #creer un dictionnaire qui prend comme valeur le nom et le prenom de l'etudiant et comme valeur une liste de ces notes par matiere
+        student_dictionnary.append({"Nom":etudiant,"Matricule":matricule,"Notes":note_list ,"Moyenne:":moy_G ,"Mention":mentions})
+    return pd.DataFrame(student_dictionnary)
 
 
-n=[[12,14,5,8,10,17],[8,14,11,10,13,15],[8,14,17,20,19,19]]
 
-moy=calcul_mat(n)
-moy_G=calculer_moyG(moy)
+list_class , matricules=remplir_liste()
+
+df = remplir_information(list_class)
+print(df)
+
+df.to_csv("notes_etudiants.csv",index=False)
+
+
+
+
+
+# def ajouter_etu():
+#     name=input("Entrer le nom et le prenom du nouvel étudiant : ")
+#     matricule=input("Veuillez précisez le matricule de l'etudiant : ")
+#     if matricule not in matricules:
+#         matricules.append(matricule)
+#     else:
+#         print("Matricule already taken")
+#     if name not in list_class:
+#         list_class.append(name)
+#         list_sorted=sorted(list_class)
+#         return list_sorted
+#     else:
+#         return False
+
+# n=[[12,14,5,8,10,17],[8,14,11,10,13,15],[8,14,17,20,19,19]]
+
+# moy=calcul_mat(n)
+# moy_G=calculer_moyG(moy)
 #print(moy_G)
 
 # for x in n:
